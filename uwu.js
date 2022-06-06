b8d5a7b7720d85e9a07c21727f16737e1e86e655
Matyrela <67298760+Matyrela@users.noreply.github.com>
2022-06-06 11:43:37 -0300

    Create uwu.js

---
+++ uwu.js
@@ -0,0 +1,2 @@
+document.body.innerHTML = document.body.innerHTML + "<style>.uwu{cursor: url('https://res.cloudinary.com/dvkugaw3b/image/upload/v1641562028/cursor_ad12rf.png'), auto;}</style>"
+document.body.classList.add("uwu");
